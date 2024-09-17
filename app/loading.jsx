589102345d5c2cff0538e0734daed6410b53a71f
Shernille Licud <shernillelicud@Shernilles-Air.Home>
2024-09-17 16:15:38 +0200

    Implement Loading and Not Found Page

---
+++ app/loading.jsx
@@ -0,0 +1,20 @@
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const overRide = {
+  display: "block",
+  margin: "100px auto",
+};
+
+const LoadingPage = () => {
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      cssOverride={overRide}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+};
+
+export default LoadingPage;
