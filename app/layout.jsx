b70c536da14ba17c0f946665db3065a65d98fc4d
Shernille Licud <shernillelicud@Shernilles-Air.Home>
2024-09-18 17:56:27 +0200

    Setup next-auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+
 import "@/assets/styles/globals.css";
 
 export const metadata = {
@@ -10,13 +12,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
   return (
-    <html>
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,20 +1,25 @@
 import connectDB from "@/config/database";
+import mongoose from "mongoose";
 import Property from "@/models/Property";
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
 import PropertyDetails from "@/components/PropertyDetails";
 
 import Link from "next/link";
 import { FaArrowLeft } from "react-icons/fa";
+import { notFound } from "next/navigation";
 
 const ProperyPage = async ({ params }) => {
   await connectDB();
+
+  if (!mongoose.Types.ObjectId.isValid(params.id)) notFound();
+
   const property = await Property.findById(params.id).lean();
+
+  if (!property) notFound();
 
   return (
     <>
       <PropertyHeaderImage image={property.images[0]} />
-
-      {/* Back Button */}
 
       <section>
         <div className="container m-auto py-6 px-6">
@@ -22,8 +27,8 @@ const ProperyPage = async ({ params }) => {
             href="/properties"
             className="text-blue-500 hover:text-blue-600 flex items-center"
           >
-            <FaArrowLeft className="fas fa-arrow-left mr-2" /> Back to
-            Properties
+            <FaArrowLeft className="fas fa-arrow-left mr-2" />
+            Back to Properties
           </Link>
         </div>
       </section>
